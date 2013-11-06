e86451662f34a1fdfb835f41cd71885aea5b0e11
Dan Vera <dan@ianalyst.com>
2013-11-06 11:39:34 -0500

    assets

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 ruby "2.0.0"
 
 gem 'sinatra'
+gem 'sinatra-assetpack'
 gem 'json'
--- app.rb
+++ app.rb
@@ -1,5 +1,24 @@
 class App < Sinatra::Base
   
+  register Sinatra::AssetPack
+  
+  assets do
+      serve '/js', :from => 'assets/javascripts'
+      js :application, [
+        '/js/*.js'
+      ]
+    
+      serve '/css', :from => 'assets/stylesheets'
+      css :application, [
+        '/css/*.css'
+       ]
+       
+      serve '/images', from: 'assets/images'
+    
+      js_compression :jsmin
+      css_compression :sass
+  end
+  
   get '/' do
     erb :home
   end
