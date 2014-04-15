2670ed1220a4a6b2b76e47e588ce9d2f7f018e57
drkyro <dan@yovu.co>
2014-04-15 13:14:20 -0400

    <meta name="format-detection" content="telephone=no">

--- views/home.php
+++ views/home.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="format-detection" content="telephone=no">
 
     <meta name="description" content="Miami based web design firm. Creating responsive websites, great user interface and user experience, and great native apps. Available for consultation.">
     <title>Yovu</title>
