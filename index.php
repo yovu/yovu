703ab835469c7bb15a10f9dee7e0f0ee280c6be3
drkyro <dan@yovu.co>
2014-04-16 21:06:33 -0400

    implode array

--- index.php
+++ index.php
@@ -18,16 +18,21 @@
 	}
 	
 	function sendEmail() {
-		echo 'hi';
-		http_response_code(500);
-		//$mg = new Mailgun("key-4npkxa6n706uqaxa58ixijv83vdmjwo7");
-		//$domain = "sandbox77528.mailgun.org";
-    //
-		//# Now, compose and send your message.
-		//$mg->sendMessage($domain, array('from'    => 'postmaster@sandbox77528.mailgun.org', 
-		//                                'to'      => 'info@yovu.co', 
-		//                                'subject' => 'New Line From Yovu', 
-		//                                'html'    => 'Name: '.$_GET['fullname'].'<br> Email: '.$_GET['email'].' '));
+		
+		if (isset($_GET['subject'])) {
+			$subject = implode(', ', $_GET['subject']);			
+		} else {
+			$subject = 'N/A';
+		}
+		
+		$mg = new Mailgun("key-4npkxa6n706uqaxa58ixijv83vdmjwo7");
+		$domain = "sandbox77528.mailgun.org";
+    
+		# Now, compose and send your message.
+		$mg->sendMessage($domain, array('from'    => 'postmaster@sandbox77528.mailgun.org', 
+		                                'to'      => 'info@yovu.co', 
+		                                'subject' => 'New Line From Yovu', 
+		                                'html'    => 'Name: '.$_GET['fullname'].'<br> Email: '.$_GET['email'].'<br> Subject: '.$subject.' '));
 																		
 	}
 
