69987853d78442c4d5d0c6d3a872865ace9367c8
drkyro <dan@yovu.co>
2014-04-17 21:41:24 -0400

    some clients

--- views/home.php
+++ views/home.php
@@ -158,7 +158,7 @@
                 Params:
                     plugin-filter: Defines links for filtering.
                 -->
-                <nav class="submenu">
+                <nav class="submenu" style="display: none;">
                     <ul>
                         <li>
                             <a href="#" class="plugin-filter active" data-filter="all">All</a>
@@ -189,91 +189,70 @@
                 -->
                 <ul class="thumbnails plugin-filter-elements portfolio-items">
                     <li class="span4 mix illustration branding">
-                        <a href="#pages/portfolio/project_1.html" data-destination="portfolio-items" data-insert="before">
+                        <a href="#pages/portfolio/the_college_tourist.html" data-destination="portfolio-items" data-insert="before">
                             <!-- Portfolio image -->
-                            <img src="http://placehold.it/390x300" alt="Treble">
+                            <img src="/assets/images/pages/clients/thecollegetourist.png" style="width: 311px; height: 239px;" alt="Treble">
 
                             <!-- Portfolio name, activated on hover -->
                             <div class="portfolio-overlay">
-                                <h4>Vita Worldwide</h4>
+                                <h4>The College Tourist</h4>
                             </div>
                         </a>
                     </li>
+										
                     <li class="span4 mix branding typography illustration">
-                        <a href="#pages/portfolio/project_2.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
+                        <a href="#pages/portfolio/cardoneondemand.html" data-destination="portfolio-items" data-insert="before">
+                            <img src="/assets/images/pages/clients/cardoneondemand.png" style="width: 311px; height: 239px;" alt="Treble">
                             <div class="portfolio-overlay">
-                                <h4>Kokong Magazine</h4>
+                                <h4>Cardone ON-DEMAND</h4>
                             </div>
                         </a>
                     </li>
+										
                     <li class="span4 mix photography illustration">
-                        <a href="#pages/portfolio/project_3.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
+                        <a href="#pages/portfolio/multistat.html" data-destination="portfolio-items" data-insert="before">
+                            <img src="/assets/images/pages/clients/multistat.png" style="width: 311px; height: 239px;" alt="Treble">
                             <div class="portfolio-overlay">
-                                <h4>Escape The Grid 2</h4>
+                                <h4>MultiStat</h4>
                             </div>
                         </a>
                     </li>
+										
                     <li class="span4 mix photography">
-                        <a href="#pages/portfolio/project_4.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
+                        <a href="#pages/portfolio/welcometomiami.html" data-destination="portfolio-items" data-insert="before">
+                            <img src="/assets/images/pages/clients/welcometomiami.png" style="width: 311px; height: 239px;" alt="Treble">
                             <div class="portfolio-overlay">
-                                <h4>Avatar</h4>
+                                <h4>Welcome To Miami</h4>
                             </div>
                         </a>
                     </li>
+										
                     <li class="span4 mix photography typography branding">
-                        <a href="#pages/portfolio/project_5.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
+                        <a href="#pages/portfolio/impalamiami.html" data-destination="portfolio-items" data-insert="before">
+                            <img src="/assets/images/pages/clients/impalamiami.png" style="width: 311px; height: 239px;" alt="Treble">
                             <div class="portfolio-overlay">
-                                <h4>A Brighter Horizon</h4>
+                                <h4>Impala Miami</h4>
                             </div>
                         </a>
                     </li>
+										
                     <li class="span4 mix photography">
-                        <a href="#pages/portfolio/project_6.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
+                        <a href="#pages/portfolio/primecard.html" data-destination="portfolio-items" data-insert="before">
+                            <img src="/assets/images/pages/clients/primecard.png" style="width: 311px; height: 239px;" alt="Treble">
                             <div class="portfolio-overlay">
-                                <h4>The Travelers</h4>
+                                <h4>Primecard</h4>
                             </div>
                         </a>
                     </li>
-                    <li class="span4 mix illustration branding typography">
-                        <a href="#pages/portfolio/project_7.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
-                            <div class="portfolio-overlay">
-                                <h4>Tron 3</h4>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="span4 mix photography illustration">
-                        <a href="#pages/portfolio/project_8.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
-                            <div class="portfolio-overlay">
-                                <h4>Vimeo Staff Picke - Rio</h4>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="span4 mix illustration branding">
-                        <a href="#pages/portfolio/project_9.html" data-destination="portfolio-items" data-insert="before">
-                            <img src="http://placehold.it/390x300" alt="Treble">
-                            <div class="portfolio-overlay">
-                                <h4>Adobe Family. 1882</h4>
-                            </div>
-                        </a>
-                    </li>
+										
+                    
                 </ul>
             </article>
             <!-- End Article -->
 
             <!-- Footer -->
             <footer>
-                <div class="show-more-cube">
-                     <a href="#pages/portfolio/readmore.html" data-destination="plugin-filter-elements" data-insert="appendTo" data-action="remove">
-                        <span>Show<br/>More</span>
-                    </a>
-                </div>
+                
             </footer>
             <!-- End footer -->
         </div>
